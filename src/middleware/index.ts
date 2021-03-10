d24a78ef674f0f41cfbda5335198cac23fa191e6
Efe Ariaroo <efeariaroo@gmail.com>
2021-03-10 02:04:08 +0100

    remove more unneeded

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,8 +2,6 @@ import express from 'express'
 import helmet from 'helmet'
 
 import runBodyParseMiddleware from './bodyParser'
-import httpLogger from './http-logger'
-import setUncaughtExceptionHandler from './globalExceptionHandler'
 import devEnvironmentVars from './devEnvConfig'
 import setupSwagger from './swagger';
 
@@ -15,16 +13,9 @@ const iniitializeMiddlewares = (app: express.Application) => {
   
   runBodyParseMiddleware(app)
 
-  if(process.env.NODE_ENV === 'production') {
-    app.use(httpLogger)
-  }
-
   // This is key to make swagger work
-  app.use(express.static("public"))
-  
+  app.use(express.static("public"))  
   setupSwagger(app)
-
-  setUncaughtExceptionHandler(app)
 }
 
 export default iniitializeMiddlewares
